9b81bb255e340f14b6bde8f0299b7d0954f47320
bq7lm <javan121007@gmail.com>
2025-05-24 20:54:25 +0300

    fixed comments

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 # ID администратора
-ADMIN_ID = 6061124706  # Замените на свой ID
+ADMIN_ID = 6061124706  
 
 # Файл для хранения пользователей
 USER_FILE = "users.json"
